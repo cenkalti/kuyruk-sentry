52a90feebf546f8297c0606185c24f298f45223f
Cenk Alti <cenkalti@gmail.com>
2016-11-23 14:16:06 +0300

    update for kuyruk 8.0.0

--- kuyruk_sentry.py
+++ kuyruk_sentry.py
@@ -7,12 +7,10 @@ import blinker
 import raven
 from kuyruk import signals
 
-
 CONFIG = {"SENTRY_DSN": None}
 
 
 class Sentry(object):
-
     def __init__(self, kuyruk):
         kuyruk.extensions["sentry"] = self
         self.client = raven.Client(kuyruk.config.SENTRY_DSN)
@@ -20,15 +18,29 @@ class Sentry(object):
         signals.worker_failure.connect(
             self.capture_exception, sender=kuyruk, weak=False)
 
-    def capture_exception(self, sender, description, exc_info, worker, **extra):
-        sentry_id = self.client.get_ident(self.client.captureException(exc_info, extra={
-            "description": description,
-            "worker_queues": worker.queues,
-            "worker_hostname": socket.gethostname(),
-            "worker_pid": os.getpid(),
-            "worker_cmd": ' '.join(sys.argv),
-            "worker_timestamp": datetime.utcnow().isoformat()[:19],
-        }))
+    def capture_exception(self,
+                          sender,
+                          description=None,
+                          exc_info=None,
+                          worker=None,
+                          queue=None,
+                          **extra):
+        sentry_id = self.client.get_ident(
+            self.client.captureException(
+                exc_info,
+                extra={
+                    "description": description,
+                    "queue": queue,
+                    "worker_hostname": socket.gethostname(),
+                    "worker_pid": os.getpid(),
+                    "worker_cmd": ' '.join(sys.argv),
+                    "worker_timestamp": datetime.utcnow().isoformat()[:19],
+                }))
         description["sentry_id"] = sentry_id
-        self.on_exception.send(sender, description=description,
-                               exc_info=exc_info, worker=worker, **extra)
+        self.on_exception.send(
+            sender,
+            description=description,
+            exc_info=exc_info,
+            worker=worker,
+            queue=queue,
+            **extra)
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(
     name='Kuyruk-Sentry',
-    version="1.1.1",
+    version="1.1.2",
     author=u'Cenk Altı',
     author_email='cenkalti@gmail.com',
     keywords='kuyruk sentry',
     url='https://github.com/cenkalti/kuyruk-sentry',
     py_modules=["kuyruk_sentry"],
     install_requires=[
-        'kuyruk>=6.0.0',
+        'kuyruk>=8.0.0',
         'raven>=5.1.1',
     ],
     entry_points={'kuyruk.config': 'sentry = kuyruk_sentry:CONFIG'},
