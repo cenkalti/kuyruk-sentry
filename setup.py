c31c27888a76461a73795c0c81846b0f6afac782
Cenk Alti <cenkalti@gmail.com>
2017-04-15 17:42:54 +0300

    python 3 compatibility

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='Kuyruk-Sentry',
-    version="1.1.2",
+    version="1.1.3",
     author=u'Cenk Altı',
     author_email='cenkalti@gmail.com',
     keywords='kuyruk sentry',
@@ -16,4 +16,14 @@ setup(
     entry_points={'kuyruk.config': 'sentry = kuyruk_sentry:CONFIG'},
     description='Sends exceptions in Kuyruk workers to Sentry.',
     long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Object Brokering',
+        'Topic :: System :: Distributed Computing',
+    ],
 )
