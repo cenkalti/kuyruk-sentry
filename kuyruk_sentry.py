639b335991ff8d0da8641450f012655654c22c3d
BatuAksoy <aksoyb15@itu.edu.tr>
2020-07-03 18:36:43 +0300

    update for new sentry sdk and kuyruk 9.4.0

--- kuyruk_sentry.py
+++ kuyruk_sentry.py
@@ -4,7 +4,7 @@ import socket
 from datetime import datetime
 
 import blinker
-import raven
+import sentry_sdk
 from kuyruk import signals
 
 CONFIG = {"SENTRY_DSN": None}
@@ -13,7 +13,7 @@ CONFIG = {"SENTRY_DSN": None}
 class Sentry(object):
     def __init__(self, kuyruk):
         kuyruk.extensions["sentry"] = self
-        self.client = raven.Client(kuyruk.config.SENTRY_DSN)
+        sentry_sdk.init(kuyruk.config.SENTRY_DSN)
         self.on_exception = blinker.Signal()
         signals.worker_failure.connect(
             self.capture_exception, sender=kuyruk, weak=False)
@@ -25,22 +25,25 @@ class Sentry(object):
                           worker=None,
                           queue=None,
                           **extra):
-        sentry_id = self.client.get_ident(
-            self.client.captureException(
-                exc_info,
-                extra={
-                    "description": description,
-                    "queue": queue,
-                    "worker_hostname": socket.gethostname(),
-                    "worker_pid": os.getpid(),
-                    "worker_cmd": ' '.join(sys.argv),
-                    "worker_timestamp": datetime.utcnow().isoformat()[:19],
-                }))
+        with sentry_sdk.push_scope() as scope:
+            extras = {
+                        "description": description,
+                        "queue": queue,
+                        "worker_hostname": socket.gethostname(),
+                        "worker_pid": os.getpid(),
+                        "worker_cmd": ' '.join(sys.argv),
+                        "worker_timestamp": datetime.utcnow().isoformat()[:19],
+                    }
+            for key, value in extras.items():
+                scope.set_extra(key, value)
+            sentry_id = sentry_sdk.capture_exception(exc_info)
         description["sentry_id"] = sentry_id
-        self.on_exception.send(
-            sender,
-            description=description,
-            exc_info=exc_info,
-            worker=worker,
-            queue=queue,
-            **extra)
+        # existence of sentry_id means exception is accepted by sentry
+        if sentry_id:
+            self.on_exception.send(
+                sender,
+                description=description,
+                exc_info=exc_info,
+                worker=worker,
+                queue=queue,
+                **extra)
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import mock
 
 import kuyruk
 import kuyruk_sentry
+import sentry_sdk
 
 c = kuyruk.Config()
 c.SENTRY_DSN = "..."
@@ -12,26 +13,43 @@ c.SENTRY_DSN = "..."
 k = kuyruk.Kuyruk(c)
 
 
-@k.task
+@k.task()
 def error():
     1/0
 
 
-Args = namedtuple("Args", ["queue", "local"])
+Args = namedtuple("Args", ["queues", "local", "logging_level",
+                           "max_run_time", "max_load"])
+Args.__new__.__defaults__ = (None,) * len(Args._fields)
 
 
 class SentryTestCase(unittest.TestCase):
+    def test_init(self):
+        sentry_sdk.init = mock.Mock()
+        try:
+            kuyruk_sentry.Sentry(k)
+        except sentry_sdk.utils.BadDsn:
+            pass
 
-    @mock.patch("kuyruk_sentry.raven")
-    def test_save_exception(self, mock_raven):
-        s = kuyruk_sentry.Sentry(k)
+        assert sentry_sdk.init.called
 
-        queue = "kuyruk"
+    def test_save_exception(self):
+        sentry_sdk.capture_exception = mock.Mock()
+
+        try:
+            kuyruk_sentry.Sentry(k)
+        except sentry_sdk.utils.BadDsn:
+            pass
+
+        queues = "kuyruk"
         args, kwargs = (), {}
-        desc = error._get_description(args, kwargs, queue)
+        desc = error._get_description(args, kwargs)
 
-        message = mock.Mock()
-        w = kuyruk.Worker(k, Args(queue, False))
+        message = mock.Mock(
+            delivery_info={'routing_key': None},
+            channel=mock.Mock()
+        )
+        w = kuyruk.Worker(k, Args(queues, False))
         w._process_task(message, desc, error, args, kwargs)
 
-        assert s.client.captureException.called
+        assert sentry_sdk.capture_exception.called
